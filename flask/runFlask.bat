c7f0a9141a2aaec7d0e4877d381609a82c3fb07d
Satoru Takagi <satakagi@users.noreply.github.com>
2019-02-21 16:55:44 +0900

    Add files via upload

---
+++ flask/runFlask.bat
@@ -0,0 +1,5 @@
+REM https://auth0.com/blog/jp-developing-restful-apis-with-python-and-flask/
+set FLASK_APP=helloFlask.py
+REM set FLASK_APP=./cashman/index.py
+set FLASK_ENV=development
+flask run -h 0.0.0.0
