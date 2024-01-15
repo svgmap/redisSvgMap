23f492fbc97bcb6ca73e5fc14aca27caf791261f
jakkyfc <jakky.lin@gmail.com>
2024-01-15 13:59:24 +0900

    全体的にMockの適用方法を修正

--- tests/test_flaskmain.py
+++ tests/test_flaskmain.py
@@ -11,7 +11,7 @@ import pickle
 
 class TestOfFlaskApps(unittest.TestCase):
   def setUp(self):
-    self.main = app.test_client();
+    self.main = app.test_client()
 
   def test_access2RootPath(self):
     response = self.main.get("/")
@@ -26,20 +26,24 @@ class TestOfFlaskApps(unittest.TestCase):
     response = self.main.get("/dataset/show")
     self.assertEqual(response.status_code, 200)
 
-  @patch("scripts.csv2redis.Csv2redisClass.registSchema", autospec=True)
-  @patch("scripts.csv2redis.Csv2redisClass.__init__", autospec=True)
-  def test_buildLayer(self,mock_c2rini, mock_c2rreg):
-    mock_c2rini.return_value = None
-    mock_c2rreg.return_value = True
+  @patch("flaskmain.Csv2redisClass", autospec = True)
+  def test_buildLayer(self,mock_c2r):
+    mock_c2r.return_value.registSchema.return_value = True
     postData = {'schema': ['title', 'metadata', 'latitude', 'longitude'], 'type': [2, 2, 1, 1], 'latCol': 2, 'lngCol': 3, 'titleCol': 0, 'idCol': -1, 'namespace': 'abcdefg_', 'name': 'sample', 'created': 1703751389604, 'defaultIcon': 8, 'defaultIconPath': 'pngs/pin_red_cross.png'}
     response = self.main.post("/svgmap/buildLayer", data=json.dumps(postData), content_type='application/json')
     self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.data, b"OK")
+    response.close()
+    mock_c2r.return_value.registSchema.return_value = False
+    postData = {'schema': ['title', 'metadata', 'latitude', 'longitude'], 'type': [2, 2, 1, 1], 'latCol': 2, 'lngCol': 3, 'titleCol': 0, 'idCol': -1, 'namespace': 'abcdefg_', 'name': 'sample', 'created': 1703751389604, 'defaultIcon': 8, 'defaultIconPath': 'pngs/pin_red_cross.png'}
+    response = self.main.post("/svgmap/buildLayer", data=json.dumps(postData), content_type='application/json')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.data, b"DUPLICATED ERROR")
     response.close()
 
-  @patch("scripts.csv2redis.Csv2redisClass.__init__", autospec=True)
-  def test_access2IndexFile(self, mock_c2rini):
-    mock_c2rini.return_value = None
-    print("static index file ")
+
+  @patch("flaskmain.Csv2redisClass", autospec = True)
+  def test_access2IndexFile(self, mock_c2r):
     response = self.main.get("/svgmap/index.html")
     self.assertEqual(response.status_code, 200)
     response.close()
@@ -50,9 +54,8 @@ class TestOfFlaskApps(unittest.TestCase):
     self.assertEqual(response.status_code, 200)
     response.close()
 
-  @patch("scripts.csv2redis.Csv2redisClass.__init__", autospec=True)
-  def test_access2StaticImageFile(self, mock_c2rini):
-    mock_c2rini.return_value = None
+  @patch("flaskmain.Csv2redisClass", autospec = True)
+  def test_access2StaticImageFile(self, mock_c2r):
     response = self.main.get("/svgmap/pngs/pin_yellow.png")
     self.assertEqual(response.status_code, 200)
     response.close()
@@ -60,39 +63,21 @@ class TestOfFlaskApps(unittest.TestCase):
     self.assertEqual(response.status_code, 200)
     response.close()
 
-  @patch("scripts.csv2redis.Csv2redisClass.saveSvgMapTileN", autospec=True)
-  @patch("scripts.csv2redis.Csv2redisClass.init", autospec=True)
-  @patch("scripts.csv2redis.Csv2redisClass.__init__", autospec=True)
-  def test_access2LowResImageFile(self, mock_c2rini, mock_c2rinit, mock_c2rsave):
-    mock_c2rini.return_value = None
-    mock_c2rinit.return_value = None # 問答無用でNoneにしてmockしてしまっているので、Mockの仕方がきれいじゃない
-    mock_c2rsave.return_value = ""
+  @patch("flaskmain.Csv2redisClass", autospec = True)
+  def test_access2LowResImageFile(self, mock_c2r):
     with open("./flask/webApps/Container.svg", "r") as f:
-      mock_c2rsave.return_value = f.read()
+      mock_c2r.return_value.saveSvgMapTileN.return_value = f.read()
     response = self.main.get("/svgmap/temporary/svgMapTileDB.svg")
     self.assertEqual(response.status_code, 200)
     response.close()
-  
-  @patch("pickle.loads", autospec=True)
-  @patch("redis.Redis", autospec=True)
-  @patch("scripts.csv2redis.Csv2redisClass", autospec=True)
-  def test_editPost(self, mock_c2r, mock_redis, mock_pickle):
-    mock_redis.exists.return_value = True
-    schemaObj = {
-        'schema':['Name:s', 'latitude', 'longitude'],
-        'type': [0,1,1],
-        "latCol": 1,
-        "lngCol": 2,
-        "titleCol": 0,
-        "idCol": -1,
-        "namespace": "test_",
-        "name": "default"
-    }
-    
-    mock_pickle.return_value = schemaObj
+
+  @patch("flaskmain.getData", return_value=[{"lat":36, "lng":139, "hkey":"aaaaa,bbb,ccc", "data": "aaaaa,bbb,ccc"}])
+  @patch("flaskmain.Csv2redisClass", autospec = True)
+  def test_editPost(self, mock_c2r, mock_getData):
     postData = {"action": "ADD", "to": [{"latitude":36, "longitude":139, "metadata":"aaaaa,bbb"}]}
     response = self.main.post("/svgmap/editPoint", data=json.dumps(postData), content_type='application/json')
     self.assertEqual(response.status_code, 200)
+    response.close()
 
   def test_getData(self):
     schemaObj = {
